d9115b891b2b75c843e18ebc4457269ee54150d8
Andrey Mishchenko <mishchea@gmail.com>
2022-04-18 14:32:39 -0400

    Remove coordinate system hacks

--- main.m
+++ main.m
@@ -3,20 +3,13 @@
 
 int main(int argc, const char *argv[]) {
   CGDirectDisplayID mainDisplayID = CGMainDisplayID();
-  CGSize mainDisplaySize = CGDisplayBounds(mainDisplayID).size;
-  NSLog(@"Main display id=%d, (width, height)=(%f, %f)", mainDisplayID, mainDisplaySize.width, mainDisplaySize.height);
+  // NSLog(@"Main display id=%d", mainDisplayID);
 
   while (true) {
     @autoreleasepool {
-      // Grab the current mouse location.
-      NSPoint mouseLoc = [NSEvent mouseLocation];
-      CGPoint cgPoint = NSPointToCGPoint(mouseLoc);
-      // The coordinate systems are funky.
-      cgPoint.y = mainDisplaySize.height - cgPoint.y;
-      // NSLog(@"Mouse pos: (%f, %f)", cgPoint.x, cgPoint.y);
-
-      // Grab the color at the pixel under the cursor.
-      CGRect rect = CGRectMake(cgPoint.x, cgPoint.y, 1, 1);
+      CGPoint cursor = CGEventGetLocation(CGEventCreate(NULL));
+      // NSLog(@"Mouse pos: (%f, %f)", cursor.x, cursor.y);
+      CGRect rect = CGRectMake(cursor.x, cursor.y, 1, 1);
       CGImageRef image = CGDisplayCreateImageForRect(mainDisplayID, rect);
       NSBitmapImageRep *bitmap = [[NSBitmapImageRep alloc] initWithCGImage:image];
       CGImageRelease(image);
